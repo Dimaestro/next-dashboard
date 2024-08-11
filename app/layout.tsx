d1b7ffa7170b3407476382a1a6705f16bdb33909
Dima <vidistudioproduction@gmail.com>
2024-08-11 13:20:52 +0300

    complete lesson

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import '@/app/ui/global.css';
 import { inter } from "@/app/ui/fonts";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: {
+        template: '%s | Acme Dashboard',
+        default: 'Acme Dashboard',
+    },
+    description: 'The official Next.js Course Dashboard, built with App Router.',
+    metadataBase: new URL('https://next-dashboard-zeta-five.vercel.app/'),
+};
 
 export default function RootLayout({
   children,
